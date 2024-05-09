cce2601635856fee02a52dea6787d8372c13f843
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 15:22:39 -0700

    implement error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,8 +2,13 @@ const needle = require('needle');
 const args = process.argv.slice(2);
 
 needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (err, response, body) => {
-  if (err) console.log(err);
-  console.log(response.statusCode, response.statusMessage);
+  if (err) {
+    console.log('There was a communication error!');
+    return;
+  }
+  if (response.body === 'INVALID_DATA') {
+    console.log('There was an error.');
+    return;
+  }
   console.log(body[0].description);
 });
-
