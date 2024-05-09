3ce3d44041dfd555a575dc31528e3d519ae4113d
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 15:46:52 -0700

    all done

---
+++ index.js
@@ -0,0 +1,7 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) console.log(error);
+  else console.log(desc);
+});
