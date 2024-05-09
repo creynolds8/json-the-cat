1fde930cfa7e28c8aee0fee2caf4351d7588aeaa
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 14:29:33 -0700

    implement search logic with needle

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,8 @@
 const needle = require('needle');
 
+needle.get('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (err, response, body) => {
+  if (err) console.log(err);
+  console.log(response.statusCode);
+  console.log(body);
+});
+
