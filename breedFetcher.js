f03ed53759c4ed6f51dd51265484ba242b64101f
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 15:59:45 -0700

    padd null for err in callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,14 @@ const needle = require('needle');
 const fetchBreedDescription = function(breedName, callback) {
   needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
     if (err) {
-      callback('There was a communication error!');
+      callback('There was an error.');
       return;
     }
     if (body[0] === undefined) {
       callback('There was an error.');
       return;
     }
-    callback(body[0].description);
+    callback(null, body[0].description);
   });
 };
 
