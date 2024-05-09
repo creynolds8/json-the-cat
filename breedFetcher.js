fcbf82bd6ddeddb5a4a9fe189ac8edd3eaf13986
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 14:33:29 -0700

    take cmd line arg for breed name

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,9 @@
 const needle = require('needle');
+const args = process.argv.slice(2);
 
-needle.get('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (err, response, body) => {
+needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (err, response, body) => {
   if (err) console.log(err);
-  console.log(response.statusCode);
-  console.log(body);
+  console.log(response.statusCode, response.statusMessage);
+  console.log(body[0].description);
 });
 
