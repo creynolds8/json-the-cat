d8ac4ab878f453be185da87e71439f838070b37a
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 15:33:41 -0700

    add logic into function and export

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const needle = require('needle');
 const args = process.argv.slice(2);
 
-needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (err, response, body) => {
-  if (err) {
-    console.log('There was a communication error!');
-    return;
-  }
-  if (response.body === 'INVALID_DATA') {
-    console.log('There was an error.');
-    return;
-  }
-  console.log(body[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+    if (err) {
+      callback('There was a communication error!');
+      return;
+    }
+    if (response.body === 'INVALID_DATA') {
+      callback('There was an error.');
+      return;
+    }
+    callback(body[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
