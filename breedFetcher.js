b381260552127f6b9047a6e35f4bf165c8c8aa0d
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 15:34:10 -0700

    remove cmmd line processing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,4 @@
 const needle = require('needle');
-const args = process.argv.slice(2);
 
 const fetchBreedDescription = function(breedName, callback) {
   needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
